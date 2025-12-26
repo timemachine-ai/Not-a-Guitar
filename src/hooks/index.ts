da1d0dbbfe629aeb01e6a7667419b6a7a59cec6f
Tanzim Infinity <110815775+timemachine-ai@users.noreply.github.com>
2025-12-26 18:21:24 +0600

    index html

--- src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useConfig } from './useConfig';
-export { useAudio } from './useAudio';
-export { useAutoTuner } from './useAutoTuner';
-export { useThreeJS } from './useThreeJS';
-export { useHandTracking } from './useHandTracking';
